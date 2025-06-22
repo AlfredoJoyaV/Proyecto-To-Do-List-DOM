6ac762228afe895b957ea090f2cbd73590a8189f
Erick Joya <ejoyavaz@gmail.com>
2025-06-21 20:51:32 -0600

    actualizar

--- src/header.js
+++ src/header.js
@@ -2,7 +2,6 @@ import './style.css';
 export default function createHeader() {
     const header = document.createElement('header');
     
-
     const h1 = document.createElement('h1');
     h1.textContent = "Lista de Tareas";
 
--- src/script.js
+++ src/script.js
@@ -1,19 +1,19 @@
 import createHeader from './header.js';
-
 import createFooter from './footer.js';
-const footer = createFooter(); 
-document.body.appendChild(footer); // O donde quieras agregar el footerimport './style.css'
 const app = document.getElementById('app');
 
 const header = createHeader();
 document.body.prepend(header); // O donde quieras agregar el header
+
+const footer = createFooter(); 
+document.body.appendChild(footer); // O donde quieras agregar el footerimport './style.css'
 
 //Agregar la lista de tareas
 const listasAgregadas = `
 <div class="listas">
     <h1>Agrega tu lista de tareas</h1>
     <input type="text" id="listaInput" placeholder="Escribe el nombre de la lista">
-    <button id="agregarLista">Agregar Lista</button>
+    <button id="agregarLista">Agregar Tareas</button>
 </div>    
 `;
 
