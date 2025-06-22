e366b8a53edcc71bef242a6a0d9f25bef95edda5
Carlos8965 <carloscarbajal8965@gmail.com>
2025-06-22 12:35:34 -0600

    :art: Se agrego un fondo a las tareas y se modifico el footer

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ export default function createFooter() {
     const footer = document.createElement('footer');
     
     const p = document.createElement('p');
-    p.textContent = "TADA ENTERPRISES © 2023 All rights reserved.";
+    p.textContent = "TADA ENTERPRISES © 2025 All rights reserved.";
 
     footer.appendChild(p);
     return footer;
